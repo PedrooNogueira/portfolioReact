805c426c60287f6d35421b96f755d2cee64766c5
pedro2350 <pedrohenrique70866@gmail.com>
2024-02-01 00:13:14 -0300

    componentes-jsx

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css'
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            Feito com carinho por Pedro Nogueira &copy; 2023
+        </footer>
+    )
+}
+
+export default Footer
