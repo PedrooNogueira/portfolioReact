3b8fa48a9d847924bd333a9a0c2b327c18e00f42
pedro2350 <pedrohenrique70866@gmail.com>
2024-02-01 22:27:37 -0300

    componentes - aninhados

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,32 +1,23 @@
-import Header from '../../components/Header'
-import Footer from '../../components/Footer'
 import { Link } from 'react-router-dom';
-import Container from '../../components/Container';
 import styles from './Home.module.css'
 
 function Home() {
     return (
-        <>
-            <Header />
-            <Container>
-                <section className={styles.home}>
-                    <div className={styles.apresentacao}>
-                        <p>
-                            Olá, sou <br />
-                            <span>Pedro Nogueira</span> <br />
-                            Estudante de TI
-                        </p>
-                        <Link to="/sobre" className={`${styles.btn} ${styles.btn_green}`}>
-                            Sobre mim
-                        </Link>
-                    </div>
-                    <figure>
-                        <img className={styles.img_home} src="/undrawn.svg" alt="imagem home" />
-                    </figure>
-                </section>
-            </Container>
-            <Footer />
-        </>
+        <section className={styles.home}>
+            <div className={styles.apresentacao}>
+                <p>
+                    Olá, sou <br />
+                    <span>Pedro Nogueira</span> <br />
+                    Estudante de TI
+                </p>
+                <Link to="/sobre" className={`${styles.btn} ${styles.btn_green}`}>
+                    Sobre mim
+                </Link>
+            </div>
+            <figure>
+                <img className={styles.img_home} src="/undrawn.svg" alt="imagem home" />
+            </figure>
+        </section>
     )
 }
 
