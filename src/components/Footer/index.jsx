bb5e7a8e4413838a1790370142e55a6ec32467b6
pedro2350 <pedrohenrique70866@gmail.com>
2024-02-09 17:33:15 -0300

    footer component

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,7 +3,7 @@ import styles from './Footer.module.css'
 function Footer() {
     return (
         <footer className={styles.footer}>
-            Feito com carinho por Pedro Nogueira &copy; 2023
+            Feito com carinho por Pedro Nogueira &copy; 2024
         </footer>
     )
 }
